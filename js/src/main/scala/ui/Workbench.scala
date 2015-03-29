9455b1d79a0a019cb6af6ee0f3fddee10d96f0e7
felixgborrego <felix.g.borrego@gmail.com>
2015-03-29 16:27:09 +0100

    Improve Settings page (add alert message,check connection,...)

--- js/src/main/scala/ui/Workbench.scala
+++ js/src/main/scala/ui/Workbench.scala
@@ -1,13 +1,13 @@
 package ui
 
 
-import api.{ConfigStore, DockerClient}
+import api.{ConfigStorage, DockerClient}
 import japgolly.scalajs.react.vdom.prefix_<^._
 import japgolly.scalajs.react.{BackendScope, ReactComponentB}
 import model._
 import ui.Workbench.{Backend, State}
-import ui.pages.{HomePage, Page, SettingsPage}
-import ui.widgets.{Alert, Header}
+import ui.pages.{EmptyPage, HomePage, Page, SettingsPage}
+import ui.widgets.Header
 import util.GoogleAnalytics
 import util.logger.log
 
@@ -15,7 +15,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object Workbench {
 
-  case class State(selectedPage: Page, error: Option[WorkbenchError], connection: Option[Connection])
+  case class State(selectedPage: Option[Page], connection: Option[Connection])
 
   case class Props(pages: Set[Page])
 
@@ -23,29 +23,27 @@ object Workbench {
 
     val tracker = GoogleAnalytics()
 
-    tracker.sendAppView(t.state.selectedPage.id)
 
     def show(page: Page) = t.modState { s =>
-      log.info("page: " + page + " " + s.error)
+      log.info("page: " + page)
       tracker.sendAppView(page.id)
-      Workbench.State(page, s.error, s.connection)
+      Workbench.State(Some(page), s.connection)
     }
 
-    def error(error: WorkbenchError) = {
-      log.error(error)
-      error match {
-        case NoConfigurationError => t.modState(s => State(SettingsPage, Some(error), s.connection))
-        case ex: ConnectionError => t.modState(s => State(SettingsPage, Some(error), s.connection))
-        case ex: GenericError => t.modState(s => State(s.selectedPage, Some(error), s.connection))
+    def componentWillMount(): Unit =
+      ConfigStorage.getUrlConnection().map { connection =>
+        t.modState(s =>s.copy(connection=connection))
+        connection.fold(show(SettingsPage))(_ => show(HomePage))
       }
-    }
 
-    def componentWillMount(): Unit = {
-      ConfigStore.getUrlConnection().map {
-        case None => error(NoConfigurationError)
-        case connection => t.modState(s => Workbench.State(s.selectedPage, None, connection))
+    def reconnect(): Unit =
+      ConfigStorage.getUrlConnection().map { connection =>
+        log.info("workbench reconnected to " + connection)
+        t.modState(s =>s.copy(connection=connection))
       }
-    }
+
+
+
   }
 
   def apply() = {
@@ -60,33 +58,28 @@ object WorkbenchRender {
   import Workbench._
 
   val component = ReactComponentB[Props]("Workbench")
-    .initialState(State(HomePage, None, None))
+    .initialState(State(None, None))
     .backend(new Backend(_))
     .render((P, S, B) => {
     <.div(
       Header(WorkbenchRef(S, B)),
-      S.error.map { e =>
-        Alert(e.msg, None)
-      },
-      S.selectedPage.component(WorkbenchRef(S, B))
+      S.selectedPage.map(_.component(WorkbenchRef(S, B)))
     )
   }).componentWillMount(_.backend.componentWillMount()).build
 }
 
 case class WorkbenchRef(state: State, backend: Backend) {
-  def selectedPage = state.selectedPage
+  def selectedPage = state.selectedPage.getOrElse(EmptyPage)
 
   def link(page: Page) = <.a(^.onClick --> backend.show(page), ^.href := "#")
 
   def client: Option[DockerClient] = {
-    val c = state.connection.map(DockerClient)
-    if (!c.isDefined) {
-      backend.error(NoConfigurationError)
-    }
-    c
+    state.connection.map(DockerClient)
   }
 
-  def error(error: WorkbenchError) = backend.error(error)
+  def connection: Option[Connection] = state.connection
+
+  def reconnect() = backend.reconnect()
 }
 
 
--- js/src/main/scala/ui/pages/SettingsPage.scala
+++ js/src/main/scala/ui/pages/SettingsPage.scala
@@ -1,14 +1,53 @@
 package ui.pages
 
-import japgolly.scalajs.react.ReactComponentB
+import api.{ConfigStorage, DockerClient}
 import japgolly.scalajs.react.vdom.prefix_<^._
+import japgolly.scalajs.react.{BackendScope, ReactComponentB, ReactEventI}
+import model._
 import ui.WorkbenchRef
+import ui.widgets.Alert
+import util.logger._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 object SettingsPage extends Page {
 
   val id = "Settings"
 
   case class Props(ref: WorkbenchRef)
+
+  case class State(url: String = "", error: Option[String] = None)
+
+  case class Backend(t: BackendScope[Props, State]) {
+    def onChange(e: ReactEventI) =
+      t.modState(_.copy(url = e.target.value))
+
+    def willStart(): Unit = {
+      t.props.ref.connection match {
+        case None => t.modState(s => s.copy("", Some("There is no connection configuration")))
+        case Some(c) => t.modState(s => s.copy(c.url, None))
+      }
+    }
+
+    def save(): Unit = {
+      val url = t.state.url
+      if (url.startsWith("http")) {
+        DockerClient(Connection(url)).ping().onComplete {
+          case Success(_) =>
+            t.modState(s => State(url, None))
+            ConfigStorage.saveConnection(url).map(_ => t.props.ref.reconnect())
+          case Failure(e) => {
+            log.info("Unable to connected to " + url)
+            t.modState(s => s.copy(url, Some("Unable to connected to " + url)))
+          }
+        }
+      } else {
+        log.info("Unable to connect to " + url)
+      }
+    }
+
+  }
 
   def component(ref: WorkbenchRef) = {
     val props = Props(ref)
@@ -21,16 +60,23 @@ object SettingsPageRender {
 
   import SettingsPage._
 
-  val dom = <.div(
-    <.div(^.className := "container  col-sm-10",
+  def component = ReactComponentB[Props]("SettingsPage")
+    .initialState(State())
+    .backend(new Backend(_))
+    .render((P, S, B) => {
+    vdom(S, B)
+  }).componentWillMount(_.backend.willStart())
+    .build
+
+
+  def vdom(S: State, B: Backend) = <.div(
+    S.error.map(Alert(_, None)),
+    <.div(^.className := "container  col-sm-12",
       <.div(^.className := "panel panel-default",
         <.div(^.className := "panel-heading clearfix",
-          <.h3(^.className := "panel-title pull-left", "Connection to Docker Remote Api"),
+          <.h3(^.className := "panel-title pull-left", "Connection"),
           <.div(^.className := "btn-group pull-right",
-            <.button(^.className := "btn btn-warning",
-              <.i(^.className := "fa fa-times", "Test")
-            ),
-            <.button(^.className := "btn btn-success",
+            <.button(^.className := "btn btn-success", ^.onClick --> B.save,
               <.i(^.className := "fa fa-check", "Save")
             )
           )
@@ -38,18 +84,32 @@ object SettingsPageRender {
         <.div(^.className := "modal-body",
           <.form(^.className := "form-horizontal",
             <.div(^.className := "form-group",
-              <.label(^.className := "col-xs-3 control-label", "Url"),
+              <.label(^.className := "col-xs-3 control-label", "Url", <.br(), <.small("to Docker Remote Api")),
               <.div(^.className := "col-xs-9",
-                <.input(^.`type` := "text", ^.className := "form-control", ^.value := "http://localhost:1233"
+                <.input(^.`type` := "text", ^.className := "form-control", ^.value := S.url, ^.onChange ==> B.onChange
                 )
-
               )
-            ),
-            <.div(^.className := "panel-footer",
-              <.small("Connected!!")
             )
           ),
-
+          <.div(^.className := "panel panel-default",
+            <.div(^.className := "panel-heading",
+              <.h3(^.className := "panel-title", "Mac OS config")
+            ),
+            <.div(^.className := "list-group",
+              <.div(^.className := "list-group-item",
+                <.p(^.className := "list-group-item-text",
+                  "To allow Docker ui to connect to Docker you need to allow Chrome to use the ssl credentials used by boot2Docker.",
+                  <.ul(
+                    <.li("Add credentials to your Keychain", <.br(),
+                      <.code("security import key.pem -k ~/Library/Keychains/login.keychain")
+                    ),
+                    <.li("Figure out the boot2docker ip using ", <.code("boot2docker ip")),
+                    <.li("Try to reconnect!")
+                  )
+                )
+              )
+            )
+          ),
           <.div(^.className := "panel panel-default",
             <.div(^.className := "panel-heading",
               <.h3(^.className := "panel-title", "Linux config")
@@ -69,37 +129,11 @@ object SettingsPageRender {
                 )
               )
             )
-          ),
-
-
-          <.div(^.className := "panel panel-default",
-            <.div(^.className := "panel-heading",
-              <.h3(^.className := "panel-title", "MacOS config")
-            ),
-            <.div(^.className := "list-group",
-              <.div(^.className := "list-group-item",
-                <.p(^.className := "list-group-item-text",
-                  "To allow Docker ui to connect to Docker you need to allow Chrome to use the ssl credentials used by boot2Docker.",
-                  <.ul(
-                    <.li("Add credentials to your Keychain", <.br(),
-                      <.code("security import key.pem -k ~/Library/Keychains/login.keychain")
-                    ),
-                    <.li("Try to reconnect!")
-                  )
-                )
-              )
-            )
           )
         )
       )
 
     )
   )
-
-
-  val component = ReactComponentB[Props]("SettingsPage")
-    .render((P) => {
-    dom
-  }).build
 
 }
