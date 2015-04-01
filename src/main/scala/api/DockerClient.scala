239e87364238efeac875c50e769cc1791ca0dee4
Felix Garcia Borrego <felix.g.borrego@gmail.com>
2015-04-01 21:54:11 +0100

    Add timeout to http connections

--- src/main/scala/api/DockerClient.scala
+++ src/main/scala/api/DockerClient.scala
@@ -5,14 +5,19 @@ import org.scalajs.dom.ext.Ajax
 import upickle._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Promise, Future}
+import util.logger._
 
 case class DockerClient(connection: Connection) {
+
+  val HttpTimeOut = 3 * 1000 // 3 seconds
 
   // https://docs.docker.com/reference/api/docker_remote_api_v1.17/#ping-the-docker-server
   def ping(): Future[Unit] = {
     val url = connection.url + "/_ping"
-    Ajax.get(url).map(_.responseText)
+    Ajax.get(url = url, timeout = HttpTimeOut).map { xhr =>
+      log.info("[dockerClient.ping] return: " + xhr.responseText)
+    }
   }
 
   def metadata(): Future[DockerMetadata] = {
@@ -25,34 +30,35 @@ case class DockerClient(connection: Connection) {
 
   def containersInfo(): Future[ContainersInfo] = for {
     r <- containers(all = false)
-    all  <-containers(all = true)
+    all <- containers(all = true)
   } yield ContainersInfo(r, all)
 
 
-
   // https://docs.docker.com/reference/api/docker_remote_api_v1.17/#inspect-a-container
-  def containerInfo(containerId:String): Future[ContainerInfo] ={
-    val url = connection.url + "/containers/"+containerId+"/json"
-    println("url:" + url)
-    Ajax.get(url).map { xhr =>
-      println("[dockerClient.containerInfo] return: " + xhr.responseText)
-       read[ContainerInfo](xhr.responseText)
+  def containerInfo(containerId: String): Future[ContainerInfo] = {
+    val url = connection.url + "/containers/" + containerId + "/json"
+    Ajax.get(url = url, timeout = HttpTimeOut).map { xhr =>
+      log.info("[dockerClient.containerInfo] return: " + xhr.responseText)
+      read[ContainerInfo](xhr.responseText)
     }
   }
 
-  def top(containerId:String): Future[ContainerTop] ={
-    val url = connection.url + "/containers/"+containerId+"/top"
-    Ajax.get(url).map { xhr =>
-      println("[dockerClient.top] return: " + xhr.responseText)
+    }
+  }
+
+  def top(containerId: String): Future[ContainerTop] = {
+    val url = connection.url + "/containers/" + containerId + "/top"
+    Ajax.get(url = url, timeout = HttpTimeOut).map { xhr =>
+      log.info("[dockerClient.top] return: " + xhr.responseText)
       read[ContainerTop](xhr.responseText)
     }
   }
 
   //https://docs.docker.com/reference/api/docker_remote_api_v1.17/#list-images
-  def images():Future[Seq[Image]] ={
+  def images(): Future[Seq[Image]] = {
     val url = connection.url + "/images/json"
-    Ajax.get(url).map { xhr =>
-      println("[dockerClient.images] return: " + xhr.responseText)
+    Ajax.get(url = url, timeout = HttpTimeOut).map { xhr =>
+      log.info("[dockerClient.images] return: " + xhr.responseText)
       read[Seq[Image]](xhr.responseText)
     }
   }
@@ -60,24 +66,24 @@ case class DockerClient(connection: Connection) {
   // https://docs.docker.com/reference/api/docker_remote_api_v1.17/#list-containers
   private def containers(all: Boolean): Future[Seq[Container]] = {
     val url = connection.url + "/containers/json?all=" + all
-    Ajax.get(url).map { xhr =>
-      println("[dockerClient.containers] return: " + xhr.responseText)
+    Ajax.get(url = url, timeout = HttpTimeOut).map { xhr =>
+      log.info("[dockerClient.containers] return: " + xhr.responseText)
       read[Seq[Container]](xhr.responseText)
     }
   }
 
   private def info(): Future[Info] = {
     val url = connection.url + "/info"
-    Ajax.get(url).map { xhr =>
-      println("[dockerClient.info] return: " + xhr.responseText)
+    Ajax.get(url = url, timeout = HttpTimeOut).map { xhr =>
+      log.info("[dockerClient.info] return: " + xhr.responseText)
       read[Info](xhr.responseText)
     }
   }
 
   private def version(): Future[Version] = {
     val url = connection.url + "/version"
-    Ajax.get(url).map { xhr =>
-      println("[dockerClient.version] return: " + xhr.responseText)
+    Ajax.get(url = url, timeout = HttpTimeOut).map { xhr =>
+      log.info("[dockerClient.version] return: " + xhr.responseText)
       read[Version](xhr.responseText)
     }
   }
